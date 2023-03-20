addcb09b27222c8997b57f8f67a38205f60b31d3
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2023-03-20 23:23:23 +0300

    Создал header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,3 +1,35 @@
 export const Navbar = () => {
-  return (<p>Navbar</p>)
+  return (
+    <div className='container px-20'>
+      <div className='flex justify-between py-4'>
+        <a className=' my-auto'>
+          <p className="font-cressida text-5xl">Драйв</p>
+        </a>
+        <div className="flex justify-between gap-20">
+          <a href="#" className='my-auto text-xl p-4 hover:text-cyan-700 ease-in-out duration-150'>
+            Афиша
+          </a>
+
+          <a href="#" className='my-auto p-4 text-xl hover:text-cyan-700 ease-in-out duration-150'>
+            Сеть
+          </a>
+
+          <a href="#" className='my-auto p-4 text-xl hover:text-cyan-700 ease-in-out duration-150'>
+            Акции
+          </a>
+
+          <a href="#" className='my-auto p-4 text-xl hover:text-cyan-700 ease-in-out duration-150'>
+            Новости
+          </a>
+        </div>
+
+        <a className='my-auto text-xl p-4 rounded hover:bg-cyan-700 hover:text-white ease-in-out duration-300' href="#">
+          <p>Вход</p>
+        </a>
+
+
+      </div>
+      <hr/>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cressida : ["CressidaSwashC", "sans-serif"],
+        serif: ["Nunito", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
